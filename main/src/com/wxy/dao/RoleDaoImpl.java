63a6d6503617fde62df53936c75812d0547dff67
Mikael <1971364412@qq.com>
2022-11-20 18:43:13 +0800

    测试版本

--- main/src/com/wxy/dao/RoleDaoImpl.java
+++ main/src/com/wxy/dao/RoleDaoImpl.java
@@ -14,14 +14,14 @@ import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
-public class RoleDaoImpl implements RoleDao{
+public class RoleDaoImpl implements RoleDao {
     private static Connection conn;
     private PreparedStatement ps;
     private ResultSet rs;
 
 
     @Override
-    public List<Role> selectRoleByAll(){
+    public List<Role> selectRoleByAll() {
         conn = JdbcUtils.getConnection();
         //创建集合存放数据
         List roles = new ArrayList<>();
@@ -32,7 +32,7 @@ public class RoleDaoImpl implements RoleDao{
         try {
             ps = conn.prepareStatement(sql);
             rs = ps.executeQuery(sql);
-            while(rs.next()){
+            while (rs.next()) {
                 Role role = new Role();
                 role.setR_id(rs.getInt(1));
                 role.setR_name(rs.getString(2));
@@ -49,19 +49,18 @@ public class RoleDaoImpl implements RoleDao{
     }
 
     @Override
-    public Role selectRoleByUsernameAndPassword(String username,String password){
+    public Role selectRoleByUsernameAndPassword(String username, String password) {
         conn = JdbcUtils.getConnection();
         Role role = new Role();
         String sql = "select *from role where r_username = ? and r_password = ?";
         try {
             ps = conn.prepareStatement(sql);
-            ps.setString(1,username);
-            ps.setString(2,password);
+            ps.setString(1, username);
+            ps.setString(2, password);
             rs = ps.executeQuery();
-            if(!rs.next()){
+            if (!rs.next()) {
                 System.out.println("用户名密码错误");
-            }
-            else{
+            } else {
                 role.setR_name(rs.getString(2));
                 role.setPower_id(rs.getInt(5));
             }
@@ -72,7 +71,7 @@ public class RoleDaoImpl implements RoleDao{
     }
 
     @Override
-    public List showAllProduct(){
+    public List showAllProduct() {
         conn = JdbcUtils.getConnection();
         //创建集合存放数据
         List products = new ArrayList<>();
@@ -83,7 +82,7 @@ public class RoleDaoImpl implements RoleDao{
         try {
             ps = conn.prepareStatement(sql);
             rs = ps.executeQuery(sql);
-            while(rs.next()){
+            while (rs.next()) {
                 Product product = new Product();
                 product.setPro_id(rs.getInt(1));
                 product.setPro_name(rs.getString(2));
@@ -111,9 +110,9 @@ public class RoleDaoImpl implements RoleDao{
         String sql = "insert into cart(user_id,pro_id,cart_time) VALUES(?,?,?)";
         try {
             ps = conn.prepareStatement(sql);
-            ps.setInt(1,user_id);
-            ps.setInt(2,productId);
-            ps.setString(3,df.format(date));
+            ps.setInt(1, user_id);
+            ps.setInt(2, productId);
+            ps.setString(3, df.format(date));
 
             res = ps.executeUpdate();
         } catch (SQLException e) {
@@ -139,11 +138,11 @@ public class RoleDaoImpl implements RoleDao{
         String sql = "select sum(cart_money),count(cart_id) from cart WHERE user_id = ?";
         try {
             ps = conn.prepareStatement(sql);
-            ps.setInt(1,user_id);
-            rs =  ps.executeQuery();
-            if (!rs.next()){
+            ps.setInt(1, user_id);
+            rs = ps.executeQuery();
+            if (!rs.next()) {
                 System.out.println("你尚未添加任何商品，请先添加至购物车或者直接购买");
-            }else {
+            } else {
                 cartNum = rs.getInt(2);
                 cartMoney = rs.getDouble(1);
 //                根据不同的商品编号找到不同的商家，进行统计分类(忽略)
@@ -151,27 +150,27 @@ public class RoleDaoImpl implements RoleDao{
 //                通过user_id获取一下地址
 
 //                生成订单，然后清除购物车
-                sql = "insert into indent(ind_id,user_id,ind_money,ind_state) values(?,?,?,?)";
                 Date date = new Date();
                 SimpleDateFormat df = new SimpleDateFormat("yyyyMMddhhmmss");
-                ind_id = UUID.randomUUID()+ df.format(date);
+                ind_id = UUID.randomUUID() + df.format(date);
+                String sql1 = "insert into indent(ind_id,user_id,ind_money,ind_state) values(?,?,?,?)";
+                ps = conn.prepareStatement(sql1);
 
-                ps = conn.prepareStatement(sql);
-                ps.setString(1,ind_id);
-                ps.setInt(2,user_id);
-                ps.setDouble(3,cartMoney);
-                ps.setInt(4,1);
+                ps.setString(1, ind_id);
+                ps.setInt(2, user_id);
+                ps.setDouble(3, cartMoney);
+                ps.setInt(4, 1);
 
                 int intIndent = ps.executeUpdate();
-                if (intIndent>0){
+                if (intIndent > 0) {
 //                    清除购物车
                     sql = "DELETE FROM cart WHERE user_id = ?";
                     ps = conn.prepareStatement(sql);
-                    ps.setInt(1,user_id);
+                    ps.setInt(1, user_id);
                     int cartRes = ps.executeUpdate();
-                    if (cartRes != cartNum){
+                    if (cartRes != cartNum) {
                         return 0;
-                    }else {
+                    } else {
                         return cartNum;
                     }
                 }
